752917b893d893e0b020513e710b1da4cde563c7
supra1nova <supra1nova@gmail.com>
2022-09-27 12:53:14 +0900

    initial commit

---
+++ src/test/java/com/selfdriven/semo/SemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfdriven.semo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
